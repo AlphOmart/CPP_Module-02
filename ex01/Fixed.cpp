78299e139c398e581fe88a09235e9d35031e5800
AlphomArt <menelik.wubneh@gmail.com>
2023-10-04 13:57:33 +0200

    I didn't understand some things, but seems to works, need to fixs the output and exec name

---
+++ ex01/Fixed.cpp
@@ -0,0 +1,64 @@
+#include "Fixed.hpp"
+
+Fixed::Fixed( void )
+{
+	_nbr = 0;
+	std::cout << "Default constructor called" << std::endl;
+}
+
+Fixed::Fixed( Fixed& original )
+{
+	_nbr = original._nbr;
+	std::cout << "Copy constructor called" << std::endl;
+}
+
+Fixed::Fixed( const int nbr )
+{
+	std::cout << "Int constructor called" << std::endl;
+	_nbr = nbr * (1 << this->_fractional_bits);
+}
+
+Fixed::Fixed(const float nb):
+		_nbr(roundf(nb * (1 << this->_fractional_bits))) {
+	std::cout << "Float constructor called" << std::endl;
+}
+Fixed::~Fixed( void )
+{
+	std::cout << "Destructor called" << std::endl;
+}
+
+Fixed&	Fixed::operator=(const Fixed &original)
+{
+	std::cout << "Copy assignment operator called" << std::endl;
+	if (&original == this)
+		return (*this);
+	this->setRawBits(original.getRawBits());
+	return (*this);
+}
+
+int		Fixed::getRawBits( void ) const
+{
+	std::cout << "getRawBits member function called" << std::endl;
+	return (this->_nbr);
+}
+
+void	Fixed::setRawBits( int const raw )
+{
+	std::cout << "setRawBits member function called" << std::endl;
+	this->_nbr = raw;
+}
+
+float	Fixed::toFloat(void) const {
+	return (float)this->_nbr/ (1 << this->_fractional_bits);
+}
+
+int		Fixed::toInt( void ) const
+{
+	return (this->_nbr >> this->_fractional_bits);
+}
+
+std::ostream&	operator<<(std::ostream& os, const Fixed& fixed_point)
+{
+	os << fixed_point.toFloat();
+	return os;
+}
---
+++ ex01/Fixed.hpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <cmath>
+
+
+class Fixed
+{
+private:
+	static const int	_fractional_bits = 8;
+	int					_nbr;
+
+public:
+	Fixed( void );
+	Fixed( Fixed& original );
+	Fixed( const int original );
+	Fixed( const float original );
+	~Fixed( void );
+
+	Fixed& operator=(const Fixed& original);
+
+	int		getRawBits( void )const;
+	void	setRawBits( int const raw );
+	float	toFloat( void )const;
+	int		toInt( void )const;
+};
+
+std::ostream&	operator<<(std::ostream& os, const Fixed& fixed_point);
