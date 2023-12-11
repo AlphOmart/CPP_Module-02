92563f7f18d0f558b8734bef9b5579c862a72772
AlphomArt <menelik.wubneh@gmail.com>
2023-12-11 18:44:31 +0100

    seems to be ok

--- ex00/Fixed.hpp
+++ ex00/Fixed.hpp
@@ -1,3 +1,6 @@
+#ifndef FIXED_HPP
+# define FIXED_HPP
+
 #include <iostream>
 
 class Fixed
@@ -16,3 +19,5 @@ public:
 	int		getRawBits( void )const;
 	void	setRawBits( int const raw );
 };
+
+#endif
